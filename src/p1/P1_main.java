6dd68d7d889684d00e3f73a844bc4ca516a5506c
Son <son.trinhh07@gmail.com>
2025-11-20 22:33:18 +0100

    1. Hausaufgabe

---
+++ src/p1/P1_main.java
@@ -0,0 +1,21 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		int x;
+		
+		for (x = 1; x <= 11; x++) {
+			System.out.println(x);
+		}
+		
+		x=0;
+do {
+	x++;
+	System.out.println(x);
+   } while (x <= 10);
+
+}
+	}
+
+
